f7d57bfe62ba4d007f3103ea94fe8eb6adbebb2f
Khalela Stevens <khalelau.stevens@student.fairfield.edu>
2020-12-17 12:26:51 -0500

    new

--- LibraryManagement/server.js
+++ LibraryManagement/server.js
@@ -9,3 +9,17 @@ app.get('/*', function(req,res) {
 
 app.listen(process.env.PORT || 8080);
 }
+
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
